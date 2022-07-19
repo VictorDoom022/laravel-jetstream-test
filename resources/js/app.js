f15918180c1e04492254304f60d22bb73dff1d2f
unknown <victordoom022@gmail.com>
2022-07-19 17:52:05 +0800

    install sweetalert

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,13 @@
 import './bootstrap';
 import '../css/app.css';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import VueSweetalert2 from 'vue-sweetalert2';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -16,6 +18,7 @@ createInertiaApp({
         return createApp({ render: () => h(app, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .use(VueSweetalert2)
             .mount(el);
     },
 });
